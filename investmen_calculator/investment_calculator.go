0b5b736be4efb1fb76997adf6e407b38e1f7cd94
auronvila <auronvila1121@gmail.com>
2024-07-29 10:33:02 +0300

    return statements refactoring

--- investmen_calculator/investment_calculator.go
+++ investmen_calculator/investment_calculator.go
@@ -5,8 +5,9 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
-	const inflationRate = 2.5
 	var investmentAmount, years, expectedReturnRate float64
 
 	fmt.Print("Please enter an investment amount: ")
@@ -16,11 +17,17 @@ func main() {
 	fmt.Print("Please enter the expected return rate: ")
 	fmt.Scan(&expectedReturnRate)
 
-	futureVal := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealVal := futureVal / math.Pow(1+inflationRate/100, years)
+	futureVal, futureRealVal := calculateValues(investmentAmount, expectedReturnRate, years)
 
-	// using sprintf to return the string that we are formating and using it to print to the terminal.
+	// using sprintf to return the string that we are formating and using it to print the message to the terminal.
 	formattedOutput := fmt.Sprintf("Future value: %.3f \nFuture Val adjusted for inflation: %.3f", futureVal, futureRealVal)
 
 	fmt.Printf(formattedOutput)
+}
+
+func calculateValues(investmentAmount float64, expectedReturnRate float64, years float64) (futureVal float64, futureRealVal float64) {
+	futureVal = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealVal = futureVal / math.Pow(1+inflationRate/100, years)
+
+	return futureVal, futureRealVal
 }
