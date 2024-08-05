f89e2478866e060141b25c9adcd9ace6ff038d9d
auronvila <auronvila1121@gmail.com>
2024-08-05 23:16:54 +0300

    maps in go,

---
+++ maps/go.mod
@@ -0,0 +1,3 @@
+module github.com/maps
+
+go 1.22
---
+++ maps/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	websites := map[string]string{
+		"google":              "www.google.com",
+		"amazon web services": "www.aws.com",
+	}
+	fmt.Println(websites["google"])
+}
